3ce94aa74ae278a629f406785d35acfea0dcfd19
J Mulder <jlmulder2014@gmail.com>
2020-06-17 17:26:26 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,17 +156,26 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     
   },
+
   deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
    
   },
   over: function(event) {
-   
+   $(event.target).addClass("dropover-active");
+  
   },
+
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
     
   },
+  
   update: function(event) {
    // array to store the task data in
     var tempArr = [];
@@ -206,12 +215,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -238,7 +250,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
-
+  console.log(taskEl)
 };
 
 // modal was triggered
@@ -254,7 +266,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -287,5 +299,12 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, 1800000);
 
-
+// setInterval(function() {
+//   // code to execute
+// }, (1000 * 60) * 30);
